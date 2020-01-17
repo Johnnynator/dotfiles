00bbcd67aae1ee046a42a07a9fe3e83f94b9a975
John Zimmermann <johnz@posteo.net>
2020-01-17 16:09:24 +0100

    [config] fish: add StarConflict completions

---
+++ .config/fish/completions/StarConflict.fish
@@ -0,0 +1,42 @@
+# StarConflict
+
+function get_files -d 'get packaged files' --argument filter
+	set -l cache_file /tmp/.starconflict-cache.$USER
+	if test -f $cache_file
+		cat $cache_file | grep $filter
+		set -l age (math (date +%s) - (stat -c '%Y' $cache_file))
+		set -l max_age 3600
+		if test $age -lt $max_age
+			return
+		end
+	end
+	quickbms -l /home/john/.local/share/quickbms/clutch.bms data/ 2>/dev/null | awk '{print $3}' >$cache_file &
+end
+
+complete -f -c StarConflict
+complete -f -c StarConflict -o m -l model -r -a '(get_files '\\.mdf\\\$')' -d 'Load model'
+complete -f -c StarConflict -o s -l scene -r -a '(get_files '\\.map\\\$')' -d 'Load scene'
+# complete -f -c StarConflict -o p -l partcl -d '?'
+# complete -f -c StarConflict -o rd -l ragdoll -d '?'
+# complete -f -c StarConflict -opr -l prototype -d '?'
+complete -f -c StarConflict -o nosteam -d 'Start without Steam integration'
+complete -f -c StarConflict -l lang -d 'Choose language' \
+	-r -a 'chinese czech english french german hungarian polish portuguese russian spanish turkish ukrainian vietnamese'
+complete -f -c StarConflict -l daemonize -d 'Daemonize after startup'
+complete -c StarConflict -l pidfile -d 'Pidfile' -r
+complete -c StarConflict -o xterm -d 'Start seperate xterm to show stdout'
+# complete -c StarConflict -o from_xterm -d '?'
+complete -f -c StarConflict -o test -d 'Show server browser'
+complete -f -c StarConflict -o pubtest -d 'Set default server to pubtest'
+complete -f -c StarConflict -o dontSaveConfig -d 'Don\'t save config after closing'
+complete -f -c StarConflict -o skiplogos -d 'Skip over startup logos'
+complete -f -c StarConflict -o safe -d 'Start SC with default config'
+# complete -f -c StarConflict -o noMenu -d ''
+# complete -f -c StarConflict -o nompcheck -d ''
+# complete -f -c StarConflict -o autoLogin -d ''
+# complete -f -c StarConflict -o anim -d ''
+# complete -f -c StarConflict -o logFileName -d ''
+# complete -f -c StarConflict -o logFull -d ''
+# complete -f -c StarConflict -l ignoreErrorMsg -d ''
+# complete -f -c StarConflict -o filesLog -d ''
+# complete -f -c StarConflict -o filesOneBigLog -d ''
