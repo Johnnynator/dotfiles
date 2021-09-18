65a7d95066f8a64fa76f792bba8524f292e9b2d6
John Zimmermann <me@johnnynator.dev>
2021-09-18 21:06:01 +0200

    [config] fish: add cmake env config to generate compile_commands.json

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 set fish_greeting
 set -x PATH $HOME/.bin $PATH
 set -x VAGRANT_DEFAULT_PROVIDER libvirt
+set -x CMAKE_EXPORT_COMPILE_COMMANDS 1
+set -x CMAKE_GENERATOR Ninja
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
